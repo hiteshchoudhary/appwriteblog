710d03cac9e6eb4861a3727d6c0ec9b66de37a40
Ayush <120788538+ayushsgithub@users.noreply.github.com>
2023-10-11 11:33:27 +0530

    typo corrected
    Changes made: serive => service

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -40,7 +40,7 @@ export class AuthService {
         try {
             return await this.account.get();
         } catch (error) {
-            console.log("Appwrite serive :: getCurrentUser :: error", error);
+            console.log("Appwrite service :: getCurrentUser :: error", error);
         }
 
         return null;
@@ -51,7 +51,7 @@ export class AuthService {
         try {
             await this.account.deleteSessions();
         } catch (error) {
-            console.log("Appwrite serive :: logout :: error", error);
+            console.log("Appwrite service :: logout :: error", error);
         }
     }
 }
