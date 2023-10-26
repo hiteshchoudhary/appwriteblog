03f3f26033e3abc341ab5b7fd983c7a56c98bd2a
Ayush <120788538+ayushsgithub@users.noreply.github.com>
2023-10-26 17:54:48 +0530

    Typo Update

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -12,7 +12,7 @@ function LogoutBtn() {
     }
   return (
     <button
-    className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+    className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
     onClick={logoutHandler}
     >Logout</button>
   )
