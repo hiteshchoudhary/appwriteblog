0885c2ae3a3a128eace15c8563432dd10973f1c9
devansh karamchandani <99194285+devansh-kc@users.noreply.github.com>
2023-11-10 18:30:16 +0530

    Update authSlice.js
    there should  not be action.payload.userData . because in future if someone login to the app and adds the post  there will be a bug of undefined userData.$id .

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -11,7 +11,7 @@ const authSlice = createSlice({
     reducers: {
         login: (state, action) => {
             state.status = true;
-            state.userData = action.payload.userData;
+            state.userData = action.payload;
         },
         logout: (state) => {
             state.status = false;
